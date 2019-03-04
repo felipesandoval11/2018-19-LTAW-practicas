b766d94d3d0c22923daf807087696b084d9d3990
Felipe Sandoval <felipesandovalsibada@outlook.com>
2019-03-04 12:53:29 +0100

    Practica 1

---
+++ Practica-1/Server/server.js
@@ -0,0 +1,12 @@
+// First Practice in LTAW
+// Made by Felipe Sandoval.
+
+var http = require('http');
+
+console.log("Arrancando servidor...")
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+  console.log("Peticion atendida")
+}).listen(8080);
