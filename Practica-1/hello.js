6ebae1adf261f5da2c7630c2dff0d3b2fd86ddbc
Felipe Sandoval <felipesandovalsibada@outlook.com>
2019-03-04 12:31:18 +0100

    Adding JSON package

--- Practica-1/hello.js
+++ Practica-1/hello.js
@@ -1,3 +1,3 @@
 // The first js of LTAW
-  console.log("Hey wassup");
-  process.stdout.write("Another way to print\n\n\n")
+  console.log("Hello World");
+  process.stdout.write("Another way to print.\n\n\n")
