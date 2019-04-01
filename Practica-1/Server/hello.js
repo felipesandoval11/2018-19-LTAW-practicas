33eed497d8138a698b008b57f705bb5adad5b664
Felipe Sandoval <felipesandovalsibada@outlook.com>
2019-04-01 13:13:57 +0200

    Testing

---
+++ Practica-1/Server/hello.js
@@ -0,0 +1,3 @@
+// The first js of LTAW
+  console.log("Hello World");
+  process.stdout.write("Another way to print.\n\n\n")
--- Practica-1/Server/server.js
+++ Practica-1/Server/server.js
@@ -3,10 +3,15 @@
 
 var http = require('http');
 
-console.log("Running server...")
+console.log("Running server...\n")
 
+// Creating a server object
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  console.log("Request solved.")
+  //res.write('First document solved.') // response for client
+  res.write(req.url);
+  //res.end('\.'); // end response for client
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  console.log("Testing")
 }).listen(8080);
