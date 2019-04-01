8a0b14847239ef8c07807892b993f2018f4d8065
Felipe Sandoval <felipesandovalsibada@outlook.com>
2019-04-01 13:46:42 +0200

    For different URL different responses

--- Practica-1/Server/server.js
+++ Practica-1/Server/server.js
@@ -2,6 +2,8 @@
 // Made by Felipe Sandoval.
 
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 console.log("Running server...\n")
 
@@ -9,9 +11,21 @@ console.log("Running server...\n")
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   //res.write('First document solved.') // response for client
-  res.write(req.url);
+  //res.write(req.url);
   //res.end('\.'); // end response for client
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  console.log("Testing")
+  //var q = url.parse(req.url, true).query;
+  //var txt = q.year + " " + q.month;
+  //res.write(txt);
+  //fs.readFile('doc1.html', function(err, data) {
+  //  res.writeHead(200, {'Content-Type': 'text/html'});
+  //  res.write(data);
+  //  res.end();
+  if (req.url == '/adios'){
+    res.write('Petition 1');
+  }else if (req.url == '/hola'){
+    res.write('Petition 2');
+  }else{
+    res.write('Petition 3');
+  }
+  console.log("Request Solved");
 }).listen(8080);
