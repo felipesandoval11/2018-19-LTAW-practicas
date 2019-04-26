16de475396d15d8cdd9cca948aacfc58845a386c
Felipe Sandoval <felipesandovalsibada@outlook.com>
2019-04-26 19:54:03 +0200

    New css

--- Practica-4/chat-express/server.js
+++ Practica-4/chat-express/server.js
@@ -6,13 +6,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-//--Servir la pagina principal
+const PORT = 3000;
+
+//  Get the index of my CHAT
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
   console.log("/: Showing index to user.");
   console.log("___________\n");
 });
 
+//  Get the hello html
 app.get('/hello', (req, res) => {
   res.sendFile(__dirname + '/hello.html');
   console.log("HELLO: Salute to client.");
@@ -23,38 +26,34 @@ app.get('/hello', (req, res) => {
 app.get('/chat-client.js', function(req, res){
   res.sendFile(__dirname + '/chat-client.js');
   console.log("JS requested");
+  console.log("___________\n");
 });
 
 //  Get the CSS for CHAT
 app.get('/css/micss.css', function(req, res){
   res.sendFile(__dirname + '/css/micss.css');
-  res.sendFile(__dirname + '/css/Hasthon.ttf');
-  res.sendFile(__dirname + '/css/futurist.TTF');
   console.log("CSS requested");
+  console.log("___________\n");
 });
 
-//-- Lanzar el servidor
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+//  Lauching Server
+http.listen(PORT, function(){
+  console.log('Listening on *:3000');
 });
 
-//-- Evento: Nueva conexion recibida
-//-- Un nuevo cliente se ha conectado!
+//  Handling events.
 io.on('connection', function(socket){
-  console.log('--> Usuario conectado!');
+  console.log('--> New user connected!');
 
-  //-- Detectar si el usuario se ha desconectado
+  //  Knowing if a user disconnects from the chat
   socket.on('disconnect', function(){
-    console.log('--> Usuario Desconectado');
+    console.log('--> User disconnected!');
   });
 
-  //-- Detectar si se ha recibido un mensaje del cliente
+  // If a message from a client is RECEIVED
   socket.on('new_message', msg => {
-
-    //-- Notificarlo en la consola del servidor
-    console.log("Mensaje recibido: " + msg)
-
-    //-- Emitir un mensaje a todos los clientes conectados
+    console.log("MESSAGE RECEIVED: " + msg)
+    //  that message is shown to all my clients
     io.emit('new_message', msg);
   })
 });
