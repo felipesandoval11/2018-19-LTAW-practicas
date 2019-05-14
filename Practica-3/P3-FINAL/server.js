e9b8d1e1e0eb334bfa8b6edf22b2503ab9ae389b
Felipe Sandoval <felipesandovalsibada@outlook.com>
2019-05-14 17:43:59 +0200

    Formulario implementado.

--- Practica-3/P3-FINAL/server.js
+++ Practica-3/P3-FINAL/server.js
@@ -29,7 +29,6 @@ http.createServer((req, res) => {
     case "html":
       //-- Leer las cookies
       var cookie = req.headers.cookie;
-      console.log("La Cookie: " + cookie);
       var pathname = q.pathname;
       if (pathname == '/login.html'){
         // CREATING COOKIE!
@@ -40,8 +39,87 @@ http.createServer((req, res) => {
           filename = "./error.html";
         }
       }
+      // PARA LEER LA INFO DE COMPRA
+      if (pathname == '/bill.html') {
+          if (req.method === 'POST') {
+          // Handle post info...
+          var content = `
+          <!DOCTYPE html>
+          <html lang="es" dir="ltr">
+            <head>
+              <meta charset="utf-8">
+              <title>ARTSHOP - SHOP ART ONLINE</title>
+              <link rel="stylesheet" href="/css/micss.css">
+            </head>
+            <body>
+              <div class="sticky">
+                <h1 id="mainTitle" style="font-size:6vw"> ART SHOP <h1>
+                <a href="index.html"><img class="ecom" id="ecom1" src="icon/home.png"></a>
+                <a href="login.html"><img class="ecom" id="ecom2" src="icon/search.png"></a>
+                <a href="cart.html"><img class="ecom" id="ecom3" src="icon/cart.png"></a>
+              </div>
+              <div class="log">
+                <p> <span id="cart">THIS IS YOUR BILL: </span></p>
+              </div>
+              <div class="factura">
+                <p>
+              `
+
+              /*`
+              </p>
+              </div>
+              <img id="icon" src="icon/creditcards.png">
+              <p id="footer"> <a href="https://github.com/felipesandoval11">
+                    © Felipe Sandoval
+                  </a>
+              </p>
+            </body>
+          </html>`*/
+
+          req.on('data', chunk => {
+              // Reading data and parsing it all.
+              info_form = chunk.toString().split("&");
+              var name_client = info_form[0].split("=")[1];
+              name_client = name_client.replace("+", " ")
+              var lastname_client = info_form[1].split("=")[1];
+              lastname_client = lastname_client.replace("+", " ")
+              var mail_client = info_form[2].split("=")[1];
+              mail_client = mail_client.replace("%40", "@")
+              var payment_client = info_form[3].split("=")[1];
+              //  Showing the data collected
+              console.log("Recieved from form: NAME: " + name_client + " LAST NAME: "
+                          + lastname_client + " MAIL: " + mail_client
+                          + " PAYMENT: " + payment_client);
+              content += 'NAME: ' + name_client + "<br />LAST NAME: "
+                          + lastname_client + "<br />MAIL: " + mail_client
+                          + "<br />PAYMENT: " + payment_client
+              content += `
+                </p>
+                </div>
+                <img id="icon" src="icon/creditcards.png">
+                <p id="footer"> <a href="https://github.com/felipesandoval11">
+                      © Felipe Sandoval
+                    </a>
+                </p>
+                </body>
+            </html>
+            `
+              res.statusCode = 200;
+           });
+
+           req.on('end', ()=> {
+             //-- Generar el mensaje de respuesta
+             res.setHeader('Content-Type', 'text/html')
+             res.write(content);
+             res.end();
+           })
+           return
+        }
+        break
+      }
+
+      // LAS COOKIES
       if (cookie) {
-        console.log(cookie)
         var value_cookie = cookie.split(";");
         if (value_cookie.length > 1){
           value_cookie = value_cookie[1];
@@ -49,6 +127,7 @@ http.createServer((req, res) => {
         }else{
           value_cookie = '';
         }
+        console.log("La Cookie: " + cookie);
         console.log(value_cookie);
         switch (pathname) {
           case "/buy1.html":
