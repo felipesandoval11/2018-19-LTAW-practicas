888deea4d409ee5964a8787e90b3f6f862dd0f26
Felipe Sandoval <felipesandovalsibada@outlook.com>
2019-05-15 12:04:39 +0200

    P5 TERMINADA

--- Practica-5/chat_app/client/app.js
+++ Practica-5/chat_app/client/app.js
@@ -8,7 +8,7 @@ function main() {
   // Creating WEB SOCKET
   //var socket = io();
   var recieving = false;
-  var send = document.getElementById('send')
+  //var send = document.getElementById('send')
   var display = document.getElementById('display')
   var msg = document.getElementById("msg")
 
@@ -16,22 +16,22 @@ function main() {
       if (e.keyCode === 13) { // 13 is enter
         if (msg.value != ""){
           recieving = true;
-          ipcRenderer.send('send_chat_msg', "User: " + msg.value);
+          ipcRenderer.send('send_chat_msg', msg.value);
           console.log("Message sent.");
           msg.value = "";
         }
       }
   });
 
-  send.onclick = () => {
+/*  send.onclick = () => {
 //  Sending message with event NEW_MESSAGE
     if (msg.value != ""){
       recieving = true;
-      ipcRenderer.send('new_message', "User: " + msg.value);
+      ipcRenderer.send('new_message', msg.value);
       console.log("Message sent.");
       msg.value = "";
     }
-  }
+  }*/
 
 //  Handling events. RECEIVED messages from server.
   ipcRenderer.on('Welcome', (event,msg) => {
--- Practica-5/chat_app/server/server.js
+++ Practica-5/chat_app/server/server.js
@@ -46,6 +46,12 @@ http.listen(4500, function(){
   console.log("___________");
 });
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function getDate(){
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
@@ -58,6 +64,8 @@ function getDate(){
 io.on('connection', function(socket){
   clients += 1;
   console.log('--> New user connected!');
+  // MAXIUM OF 40 USERS IN CHAT
+  var userid = getRandomInt(1,40);
   socket.emit('Welcome', 'SERVER: Welcome new user!\n');
   io.emit('new_message', 'SERVER: New user joined the chat!\n');
   //  Knowing if a user disconnects from the chat
@@ -68,30 +76,29 @@ io.on('connection', function(socket){
 
   // If a message from a client is RECEIVED
   socket.on('new_message', msg => {
-    console.log("MESSAGE RECEIVED: " + msg);
-    var msg_user =  msg.split(": ")[0];
-    var msg_txt = msg.split(": ")[1];
-    if(msg_txt == "/help"){
+    console.log("MESSAGE RECEIVED FROM USER"+ userid + ": "+ msg);
+    if(msg == "/help"){
       var ans = "SERVER:    /help = Show commands.   /list = Show how many users are connected.\n"
               + "/hello = I salute to you. /date = I show you the date.\n"
       socket.emit('new_message', ans);
       console.log('--> Sending help to an user.');
-    }else if (msg_txt == "/list") {
+    }else if (msg == "/list") {
       var ans = "SERVER: " + clients + " connected users.\n"
       socket.emit('new_message', ans);
       console.log('--> Sending list of connected users.');
-    }else if (msg_txt == "/hello") {
-      var ans = "SERVER: Hello user! I'm the server.\n"
+    }else if (msg == "/hello") {
+      var ans = "SERVER: Hello USER" + userid + "! I'm the server.\n"
       socket.emit('new_message', ans);
-      console.log('--> Sending a salute to an user.');
-    }else if (msg_txt == "/date") {
+      console.log('--> Sending a salute to user' + userid);
+    }else if (msg == "/date") {
       var today = getDate();
       var ans = "SERVER: Today is " + today + "\n"
       socket.emit('new_message', ans);
       console.log('--> Sending date to an user.');
     }else{
       //  that message is shown to all my clients
-      io.emit('new_message', msg);
+
+      io.emit('new_message', "USER" + userid + ": " + msg);
     }
   });
 });
